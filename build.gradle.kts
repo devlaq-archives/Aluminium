d5b1a47efa0ba9693ff4a8e84a248a99908343df
devlaq <devlaq4918@gmail.com>
2022-01-30 14:49:12 +0900

    Edit build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,25 +4,7 @@ plugins {
 }
 
 group = "com.github.devlaq"
-version = "1.1.3"
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/devlaq/Aluminium")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("Aluminium") {
-            from(components["java"])
-        }
-    }
-}
+version = "1.1.4"
 
 repositories {
     mavenCentral()
@@ -30,8 +12,6 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     implementation("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
 }
 
